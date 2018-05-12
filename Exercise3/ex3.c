dbb6e02c5b656a93560cba3145ae689c7467a896
Ankit-linuxMint <ankit.jagannath@student.uts.edu.au>
2018-05-12 23:44:19 +1000

    Exercise 3 Basic Functionality Completed

---
+++ Exercise3/ex3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (int argc, char * argv[])
+{
+  int age = 10;
+  int height = 72;
+
+  printf ("\n == PROGRAM OUTPUT ==\n");
+
+  printf ("\nI am %d years old\n", age);
+  printf ("I am %d inches tall\n", height);
+
+  printf ("\n ===================== \n");
+
+  return 0;
+}
