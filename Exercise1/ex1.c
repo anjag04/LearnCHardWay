145676bad756123e8ff9fdb230872a20e14c33c7
Ankit-linuxMint <ankit.jagannath@student.uts.edu.au>
2018-05-12 18:38:06 +1000

    Exercise 1 Extra Credit

--- Exercise1/ex1.c
+++ Exercise1/ex1.c
@@ -20,7 +20,12 @@ int main (int argc, char * argv[])
     %x for hexadecimal integer
     %o for octal integer
   */
-  printf ("You are %d miles away.\n", distance);
+  printf ("You are %d miles (base 10) away.\n", distance);
+  printf ("You are %x (base 16) miles away.\n", distance);
+  printf ("You are %o (base 8) miles away.\n", distance);
+  printf ("argc = %d.\n", argc);
+  for (int i = 0; i < argc; i++)
+    printf ("argv[%d] = %s.\n", i, argv[i]);
 
   return 0;
 }
